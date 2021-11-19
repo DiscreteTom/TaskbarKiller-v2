21cfe4ef2fc7c7a6f98957dbab90369e5502304a
DiscreteTom <discrete_tom@outlook.com>
2021-11-19 20:53:11 +0800

    use space for indent

--- TaskbarKiller-v2/main.c
+++ TaskbarKiller-v2/main.c
@@ -6,30 +6,30 @@ const int HOTKEY_ID = 1;
 
 int main(int argc, char **argv)
 {
-	HWND tbHdl = FindWindow("Shell_TrayWnd", NULL); // handle of taskbar
-	int hidden = 0;																	// taskbar is hidden?
-	if (RegisterHotKey(
-					NULL, // this thread will process the hotkey
-					HOTKEY_ID,
-					MOD_WIN,
-					VK_OEM_3)) // ~
-	{
-		// printf("hot key registered\n");
-	}
+  HWND tbHdl = FindWindow("Shell_TrayWnd", NULL); // handle of taskbar
+  int hidden = 0;                                  // taskbar is hidden?
+  if (RegisterHotKey(
+          NULL, // this thread will process the hotkey
+          HOTKEY_ID,
+          MOD_WIN,
+          VK_OEM_3)) // ~
+  {
+    // printf("hot key registered\n");
+  }
 
-	MSG msg = {0};
-	while (GetMessage(&msg, NULL, 0, 0) != 0) // while msg is not WM_QUIT
-	{
-		if (msg.message == WM_HOTKEY) // process hot key
-		{
-			hidden = !hidden;
-			if (hidden)
-				ShowWindow(tbHdl, 0); //hide taskbar
-			else
-				ShowWindow(tbHdl, 1); //show taskbar
-		}
-	}
+  MSG msg = {0};
+  while (GetMessage(&msg, NULL, 0, 0) != 0) // while msg is not WM_QUIT
+  {
+    if (msg.message == WM_HOTKEY) // process hot key
+    {
+      hidden = !hidden;
+      if (hidden)
+        ShowWindow(tbHdl, 0); //hide taskbar
+      else
+        ShowWindow(tbHdl, 1); //show taskbar
+    }
+  }
 
-	UnregisterHotKey(NULL, HOTKEY_ID);
-	ShowWindow(tbHdl, 1); // show taskbar at last
+  UnregisterHotKey(NULL, HOTKEY_ID);
+  ShowWindow(tbHdl, 1); // show taskbar at last
 }

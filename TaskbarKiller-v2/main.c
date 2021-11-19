eadf794294dd0be28ef2e0738b3ddc015df29db0
DiscreteTom <discrete_tom@outlook.com>
2021-11-19 20:51:54 +0800

    fix typo

--- TaskbarKiller-v2/main.c
+++ TaskbarKiller-v2/main.c
@@ -7,7 +7,7 @@ const int HOTKEY_ID = 1;
 int main(int argc, char **argv)
 {
 	HWND tbHdl = FindWindow("Shell_TrayWnd", NULL); // handle of taskbar
-	int hiden = 0;																	// taskbar is hidden?
+	int hidden = 0;																	// taskbar is hidden?
 	if (RegisterHotKey(
 					NULL, // this thread will process the hotkey
 					HOTKEY_ID,
@@ -22,8 +22,8 @@ int main(int argc, char **argv)
 	{
 		if (msg.message == WM_HOTKEY) // process hot key
 		{
-			hiden = !hiden;
-			if (hiden)
+			hidden = !hidden;
+			if (hidden)
 				ShowWindow(tbHdl, 0); //hide taskbar
 			else
 				ShowWindow(tbHdl, 1); //show taskbar

95fc39a275ec62011c9b9863e41a73d2fe6ad1f3
DiscreteTom <discrete_tom@outlook.com>
2021-11-19 20:57:58 +0800

    optimize code & debug msg

--- TaskbarKiller-v2/main.c
+++ TaskbarKiller-v2/main.c
@@ -1,35 +1,58 @@
 #include <Windows.h>
 
+#ifdef _DEBUG
+#include <stdio.h>
+#endif
+
+#ifndef _DEBUG
 #pragma comment(linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"")
+#endif
 
 const int HOTKEY_ID = 1;
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-  HWND tbHdl = FindWindow("Shell_TrayWnd", NULL); // handle of taskbar
-  int hidden = 0;                                  // taskbar is hidden?
-  if (RegisterHotKey(
-          NULL, // this thread will process the hotkey
-          HOTKEY_ID,
-          MOD_WIN,
-          VK_OEM_3)) // ~
-  {
-    // printf("hot key registered\n");
-  }
+	// get taskbar handle
+	HWND taskbar = FindWindow("Shell_TrayWnd", NULL);
+#ifdef _DEBUG
+	printf("taskbar handle: %d\n", taskbar);
+#endif
 
-  MSG msg = {0};
-  while (GetMessage(&msg, NULL, 0, 0) != 0) // while msg is not WM_QUIT
-  {
-    if (msg.message == WM_HOTKEY) // process hot key
-    {
-      hidden = !hidden;
-      if (hidden)
-        ShowWindow(tbHdl, 0); //hide taskbar
-      else
-        ShowWindow(tbHdl, 1); //show taskbar
-    }
-  }
+	// register hot key
+	if (RegisterHotKey(
+		NULL, // this thread will process the hotkey
+		HOTKEY_ID,
+		MOD_WIN,
+		VK_OEM_3 // ~
+	))
+	{
+#ifdef _DEBUG
+		printf("hot key registered\n");
+#endif
+	}
 
-  UnregisterHotKey(NULL, HOTKEY_ID);
-  ShowWindow(tbHdl, 1); // show taskbar at last
+	MSG msg = { 0 };
+	int hidden = 0;
+	while (GetMessage(&msg, NULL, 0, 0) != 0) // while msg is not WM_QUIT
+	{
+		if (msg.message == WM_HOTKEY) // process hot key
+		{
+			hidden = !hidden;
+			if (hidden) {
+				ShowWindow(taskbar, 0); //hide taskbar
+#ifdef _DEBUG
+				printf("hide\n");
+#endif
+			}
+			else {
+				ShowWindow(taskbar, 1); //show taskbar
+#ifdef _DEBUG
+				printf("show\n");
+#endif
+			}
+		}
+	}
+
+	UnregisterHotKey(NULL, HOTKEY_ID);
+	ShowWindow(taskbar, 1); // show taskbar at last
 }
